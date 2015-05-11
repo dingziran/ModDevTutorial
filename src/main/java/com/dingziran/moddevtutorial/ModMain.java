900343c36b4fdc78186e2931fc4e3f76f6902f2d
dingziran <dingziranrr@163.com>
2015-05-11 11:43:20 +0800

    custom block and item

--- src/main/java/com/dingziran/moddevtutorial/ModMain.java
+++ src/main/java/com/dingziran/moddevtutorial/ModMain.java
@@ -1,7 +1,12 @@
 package com.dingziran.moddevtutorial;
 
+import com.dingziran.moddevtutorial.block.BlockRed;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -10,18 +15,31 @@ import net.minecraft.item.Item;
 /**
  * Created by dingziran on 2015/5/6.
  */
-@Mod(modid = "ModDevTutorial", version = "0.0.1")
+@Mod(modid = "moddevtutorial", version = "0.0.1")
 public class ModMain {
-    public static CreativeTabs ModDevTab=new CreativeTabs("Mod Dev Tab") {
+    public static CreativeTabs modDevTab=new CreativeTabs("Mod Dev Tab") {
         @Override
         public Item getTabIconItem() {
             return Items.apple;
         }
     };
+    public static Block blockRed;
+    public static Item itemThingy;
+
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+ Blocks.dirt.getUnlocalizedName());
+    public void preInit(FMLPreInitializationEvent event){
+        itemThingy=new Item().setUnlocalizedName("itemThingy").setCreativeTab(modDevTab);
+        GameRegistry.registerItem(itemThingy,"itemThingy");
+
+        blockRed=new BlockRed().setBlockName("blockRed").setCreativeTab(modDevTab);
+        GameRegistry.registerBlock(blockRed,"blockRed");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
     }
 }
---
+++ src/main/java/com/dingziran/moddevtutorial/block/BlockRed.java
@@ -0,0 +1,13 @@
+package com.dingziran.moddevtutorial.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+/**
+ * Created by dingziran on 2015/5/11.
+ */
+public class BlockRed extends Block {
+    public BlockRed() {
+        super(Material.ground);
+    }
+}
