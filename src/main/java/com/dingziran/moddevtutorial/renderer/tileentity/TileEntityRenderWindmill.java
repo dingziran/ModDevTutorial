49730cd7a3b5e25c34f44376a068d6ab0dce47ef
dingziran <dingziranrr@163.com>
2015-05-19 09:43:32 +0800

    add GL11 tessellator for windmill

--- src/main/java/com/dingziran/moddevtutorial/renderer/tileentity/TileEntityRenderWindmill.java
+++ src/main/java/com/dingziran/moddevtutorial/renderer/tileentity/TileEntityRenderWindmill.java
@@ -1,14 +1,36 @@
 package com.dingziran.moddevtutorial.renderer.tileentity;
 
+import com.dingziran.moddevtutorial.ModMain;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 /**
  * Created by dingziran on 2015/5/18.
  */
 public class TileEntityRenderWindmill extends TileEntitySpecialRenderer {
+
+    private final ResourceLocation textureWindmill = new ResourceLocation(ModMain.MOD_ID,"textures/model/windmill.png");
+
+    private int textureWidth = 64;
+    private int textureHeight = 32;
+
     @Override
     public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float f) {
-        System.out.println("RENDERING BLOCK WINDMILL");
+        GL11.glPushMatrix();
+            GL11.glTranslatef((float) x, (float) y, (float) z);
+            Tessellator tessellator=Tessellator.instance;
+            this.bindTexture(textureWindmill);
+            tessellator.startDrawingQuads();
+            {
+                tessellator.addVertexWithUV(0,0,1,1,1);
+                tessellator.addVertexWithUV(0,1,1,1/4,1/4);
+                tessellator.addVertexWithUV(0,1,0,0,0);
+                tessellator.addVertexWithUV(0,0,0,0,1);
+            }
+            tessellator.draw();
+        GL11.glPopMatrix();
     }
 }
