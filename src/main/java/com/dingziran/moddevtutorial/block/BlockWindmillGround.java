959eea2138d0fab1b3f8f1c606f570d9cee44d0f
dingziran <dingziranrr@163.com>
2015-05-29 09:27:31 +0800

    add block metadata

--- src/main/java/com/dingziran/moddevtutorial/block/BlockWindmillGround.java
+++ src/main/java/com/dingziran/moddevtutorial/block/BlockWindmillGround.java
@@ -3,6 +3,8 @@ package com.dingziran.moddevtutorial.block;
 import com.dingziran.moddevtutorial.ModMain;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 /**
@@ -24,6 +26,13 @@ public class BlockWindmillGround extends Block{
     public void onBlockAdded(World world, int x, int y,int z){
         updateMultiBlockStructure(world,x,y,z);
     }
+    @Override
+    public MovingObjectPosition collisionRayTrace(World world,int x,int y,int z,Vec3 startVec, Vec3 endVec){
+        float metadata= world.getBlockMetadata(x,y,z);
+        this.setBlockBounds(0,0,0,1,(1F/16)*metadata,1);
+        return super.collisionRayTrace(world,x,y,z,startVec,endVec);
+    }
+
     public void updateMultiBlockStructure(World world,int x,int y, int z){
         isMultiBlockStructure(world,x,y,z);
     }
@@ -34,10 +43,18 @@ public class BlockWindmillGround extends Block{
             for(int z2=0;z2<3;z2++){
                 if(!mStructure) {
                     currentCheckStructure =true;
+
                     for (int x3 = 0; x3 < 3; x3++) {
                         for (int z3 = 0; z3 < 3; z3++) {
-                            if (currentCheckStructure && world.getBlock(x1 + x2 - x3, y1, z1 +z2 - z3).equals(ModMain.blockWindmillGround)) {
+                            if (currentCheckStructure && world.getBlock(x1 + x2 - x3, y1, z1 +z2 - z3).equals(ModMain.blockWindmillGround)||!(world.getBlockMetadata(x1+x2-x3,y1,z1+z2-z3)==0)) {
                                 currentCheckStructure = false;
+                            }
+                        }
+                    }
+                    if(currentCheckStructure){
+                        for (int x3 = 0; x3 < 3; x3++) {
+                            for (int z3 = 0; z3 < 3; z3++) {
+                                world.setBlockMetadataWithNotify(x1+x2-x3,y1,z1+z2-z3,x3*3+z3,2);
                             }
                         }
                     }
